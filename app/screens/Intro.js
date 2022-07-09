358d7cf8537519dcebba8ae8eef4fb851cc61f8e
Uzair Iqbal <uzair.qq@outlook.com>
2022-07-09 21:45:13 +0500

    Styling of text input and Text Styling

--- app/screens/Intro.js
+++ app/screens/Intro.js
@@ -1,22 +1,51 @@
-import { StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Dimensions,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
+import colors from "../misc/colors";
 
 const Intro = () => {
   return (
     <>
       <StatusBar hidden />
       <View style={styles.container}>
-        <Text>Enter Your Name to Continue:</Text>
-        <TextInput />
+        <Text style={styles.inputTitle}>Enter Your Name to Continue:</Text>
+        <TextInput
+          placeholder="Enter your Name to Continue"
+          style={styles.textInput}
+        />
       </View>
     </>
   );
 };
+const width = Dimensions.get("window").width - 50;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: colors.PRIMARY,
+    color: colors.PRIMARY,
+    width,
+    height: 50,
+    borderRadius: 10,
+    paddingLeft: 10,
+    fontSize: 20,
+  },
+  inputTitle: {
+    alignSelf: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 5,
+    opacity: 0.5,
   },
 });
 
